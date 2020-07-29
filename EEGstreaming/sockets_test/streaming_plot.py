4aacd0c2c4e57adad62905200fa84418b9d5c29e
mvidaldp <mvidal.dp@gmail.com>
2020-07-29 21:56:58 +0200

    Rewrite socket as websocket

--- EEGstreaming/sockets_test/streaming_plot.py
+++ EEGstreaming/sockets_test/streaming_plot.py
@@ -1,8 +1,9 @@
 import itertools
 import json
 import matplotlib.pyplot as plt
-import socket
 import time
+import asyncio
+import websockets
 
 # constants
 CHANNELS = 8  # number of eeg channels
@@ -14,7 +15,7 @@ C_SR = True  # set constant or not constant sampling rate
 # color iterable from a color map
 color = itertools.cycle(plt.get_cmap("tab10").colors)
 
-## Plot definiton
+# Plot definiton
 fig = plt.gcf()
 plt.ion()  # not sure if necessary, also works without
 plt.style.use("ggplot")  # use ggplot style
@@ -22,7 +23,7 @@ plt.style.use("ggplot")  # use ggplot style
 # prepare the lines to plot with their labels and colors
 for i in range(CHANNELS):
     c = next(color)  # get next color from the palette (color map)
-    plt.plot([], [], color=c, linewidth=0.6, linestyle="-", label=f"ch-{i + 1}")
+    plt.plot([], [], color=c, linewidth=0.6, linestyle="-", label=f"ch{i + 1}")
 
 ax = plt.gca()  # get axis
 box = ax.get_position()  # box position to shrink axis
@@ -52,8 +53,14 @@ Example:
 c_time = 0
 first = 0
 
-# TODO: improve autoscale_view and relim to have fixed grid (or set xlim manually)
-# TODO: separate storing than plotting data, start plotting when there's enoug data and plot @ 25-30FPS (e.g. store data dict by dict received, plot 25-30 times a second)
+"""TODO:
+- Improve autoscale_view and relim to have fixed grid (or set xlim manually)
+- Separate storing than plotting data, start plotting when there's enough
+data and plot @ 25-30FPS (e.g. store data dict by dict received, plot 25-30
+times a second)
+"""
+
+
 def update_lines():
     global lines
 
@@ -67,7 +74,7 @@ def update_lines():
         # series = eeg[key]["series"][selected[0] : selected[-1]]
         series = eeg[key]["series"]
         lines[i].set_data(times, series)  # update lines time-series data
-    
+
     # shrink current axis by 5% to place legend out of plot box
     ax.set_position([box.x0, box.y0, box.width * 0.95, box.height])
     # ax.set_ylim(ymin - EXTRA_YLIM, ymax + EXTRA_YLIM)
@@ -78,6 +85,7 @@ def update_lines():
     ax.autoscale_view(True, True, True)
     fig.canvas.draw()
     fig.canvas.flush_events()
+
 
 def store_data(ddict, constant=False):
     global first, c_time
@@ -103,71 +111,35 @@ def store_data(ddict, constant=False):
 
 
 # HOST = '127.0.0.1'  # standard loopback interface address (localhost)
-# HOST = socket.gethostname()  # accept all connections from the outside world
 HOST = ""  # accept all connections
 PORT = 65432  # port to listen on (non-privileged ports are > 1023)
-
-ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    # reuse address:port if on use
-    # otherwise process needs to be killed if socket wasn't closed
-    ss.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    ss.bind((HOST, PORT))  # associate the socket with an address and port
-except socket.error as e:
-    print(str(e))
-
-print("Waiting for a Connection..")
-ss.listen(5)  # listening socket, ready to accept connections
-
-c, address = ss.accept()  # wait for incoming connections
-IP = address[0]  # socket sender IP
-PORT = address[1]  # socket sender port
-print(f"Connected to {IP}:{PORT}")
-
-# vars to handle the receiving data loop
-start = False
-end = True
-chunks = ""
-no_data = 0  # no incoming data counter
 # starting time reference for elapsed time and sampling rate calculations
 t_start = None
 pkg = 0
 sampling_rate = 0  # calculated SR
-while True:  # loop for handling incoming data
-    if not t_start:  # reference timestamp not set
-        t_start = time.time()  # reference timestamp (ms)
-    t_now = time.time()  # timestamp (ms)
-    # calculate elapsed time
-    elapsed = t_now - t_start
-    # print(elapsed)
-    # read and decode incoming data (buffer of 1 byte)
-    response = c.recv(1).decode("utf-8")
-    if len(response) <= 0:  # no incoming data
-        no_data += 1  # count incoming data
-        if no_data > 10000:  # sending socket stops sending
-            break  # break loop
-    elif not start:
-        if response == "{":  # stringified JSON dict start
-            chunks += response  # add chunk
-            start = True  # dict start found flag
-            end = False  # look for the rest flag
-            no_data = 0  # reset no incoming data counter
-    elif not end:
-        if response != "}":  # stringified JSON dict end
-            chunks += response
-        else:
-            chunks += response
-            end = True  # dict start found flag
-            start = False  # look for the start flag
-            # remove ending breaklines, remove starting and ending whitespaces
-            message = chunks.rstrip("\n").strip()
-            chunks = ""  # clear accumulated chunks
-            current = json.loads(message)  # parse stringified JSON as dict
-            store_data(current, C_SR)  # add current read package for plotting
-            pkg += 1
-            sampling_rate = int(pkg / elapsed)  # calculate sampling rate 
-            # print(current)  # current received data {pkg, time, ch1-8}
-            no_data = 0
 
-ss.close()  # disconnect listening socket
-print("Socket closed")
+
+# websocket server/listener async function
+async def ws_server(websocket, path):
+    global t_start, pkg, sampling_rate  # vars to update
+    async for message in websocket:  # wait to get each message
+        if not t_start:  # reference timestamp not set
+            t_start = time.time()  # reference timestamp (ms)
+        t_now = time.time()  # timestamp (ms)
+        elapsed = t_now - t_start  # calculate elapsed time
+        current = json.loads(message)  # parse stringified JSON as dict
+        store_data(current, C_SR)  # add current read package for plotting
+        pkg += 1  # update number of messages
+        if elapsed >= 1:  # each second
+            print(f"Elapsed: {elapsed} - Packages: {pkg}")
+            sampling_rate = int(pkg / elapsed)  # calculate sampling rate
+            pkg = 0  # restart messages counter
+            t_start = time.time()  # restart time reference
+
+
+# listen for incoming websocket connections for a specific host and port
+start_server = websockets.serve(ws_server, HOST, PORT)
+# make asyncronous function run non-stop
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
+# await websocket.send(answer)  # for sending an answer or command back
